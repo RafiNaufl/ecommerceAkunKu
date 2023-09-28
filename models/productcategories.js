85bc44862dc4665024f0308be89c67926aa84745
RafiNaufl <nrafialdhawi@gmail.com>
2023-09-28 23:45:36 +0700

    update controller, model, view

--- models/productcategories.js
+++ models/productcategories.js
@@ -1,16 +1,10 @@
 'use strict';
-const {
-  Model
-} = require('sequelize');
+const { Model } = require('sequelize');
+
 module.exports = (sequelize, DataTypes) => {
   class ProductCategories extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
     static associate(models) {
-      // define association here
+      
       ProductCategories.belongsTo(models.Product, {
         foreignKey: 'ProductId'
       });
@@ -21,7 +15,7 @@ module.exports = (sequelize, DataTypes) => {
   }
   ProductCategories.init({
     ProductId: DataTypes.INTEGER,
-    CategoryId: DataTypes.INTEGER
+    CategoryId: DataTypes.INTEGER,
   }, {
     sequelize,
     modelName: 'ProductCategories',
