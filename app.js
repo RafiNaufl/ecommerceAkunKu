79a63aa482f0da95313ff236291cb02bf513b4f9
RafiNaufl <nrafialdhawi@gmail.com>
2023-09-28 19:37:08 +0700

    update data terbaru

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ const router = require('./routers/router')
 const app = express()
 const port = 3000
 
+app.set('view engine', 'ejs')
+app.use(express.urlencoded({extended: true}))
+
 // app.get('/', (req, res) => {
 //   res.send('Hello World!')
 // })
